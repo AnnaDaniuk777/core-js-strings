f86fc14710c14a8e970042fd395bed849e878c48
Anna Daniuk <danyukanya57@yandex.ru>
2025-10-28 01:11:15 +0300

    fix: delete last two functions's solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -469,23 +469,8 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(str) {
-  let result = '';
-
-  for (let i = 0; i < str.length; i = +i) {
-    const char = str[i];
-    const charCode = char.charCodeAt(0);
-
-    if (charCode >= 65 && charCode <= 90) {
-      result += String.fromCharCode(((charCode - 65 + 13) % 26) + 65);
-    } else if (charCode >= 97 && charCode <= 122) {
-      result += String.fromCharCode(((charCode - 97 + 13) % 26) + 97);
-    } else {
-      result += char;
-    }
-  }
-
-  return result;
+function encodeToRot13(/* str */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -512,22 +497,8 @@ function encodeToRot13(str) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(value) {
-  const suits = '♣♦♥♠';
-  const values = 'A234567891JQK';
-
-  if (value.startsWith('10')) {
-    const suit = value[2];
-    return 9 + suits.indexOf(suit) * 13;
-  }
-
-  const cardValue = value[0];
-  const suit = value[1];
-
-  const valueIndex = values.indexOf(cardValue);
-  const suitIndex = suits.indexOf(suit);
-
-  return valueIndex + suitIndex * 13;
+function getCardId(/* value */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
